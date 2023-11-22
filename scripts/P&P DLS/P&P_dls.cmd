4f63d59ce4dffa13f07d39542aeb30a0dfdad9e9
Rac Global <renan@racglobal.com>
2023-11-22 20:14:07 -0300

    Alterando o P&P_dls.cmd

--- scripts/P&P DLS/P&P_dls.cmd
+++ scripts/P&P DLS/P&P_dls.cmd
@@ -4,6 +4,6 @@ for /f "tokens=1-20 delims=;" %%a IN (ramais.csv) DO (
 	setlocal enabledelayedexpansion
     echo Criando o Plug and Play do ramal %%k
 	for /f %%z in ('python IPSEC_PW.py %%k') do set password=%%z
-	echo CreateSIPPhone;;%%k;;Unify SIP;;%%m;;;false;;;%%k;!password!;system;;;;;;;;;;;>> dls.csv
+	echo CreateSIPPhone;;%%d;;Unify SIP;;%%m;;;false;;;%%d;!password!;system;;;;;;;;;;;>> dls.csv
 	endlocal
 	)
